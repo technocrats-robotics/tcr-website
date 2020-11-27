c8134eadde4c51fd9b8a13e01889f6b2aaa1ec52
kiwipeace01 <60574300+kiwipeace01@users.noreply.github.com>
2020-11-27 18:24:37 +0530

    Update Page4.jsx

--- src/pages/landingPage/Page4.jsx
+++ src/pages/landingPage/Page4.jsx
@@ -1,16 +1,18 @@
+/*
 import { render } from '@testing-library/react'
 import React, { Component } from 'react';
-import {Grid, GridColumn, GridRow,Card,Icon, Image,} from 'semantic-ui-react';
+import {Grid, GridColumn, GridRow,Card,Icon, Image,Header} from 'semantic-ui-react';
 
 /**
 * @author
 * @function Page3
 **/
-
+/*
 export default class Page3 extends Component {
     render(){
   return(
     <div className="fourthPage">
+        <Header textAlign='center' color='teal'>Testimonials</Header>
         <Grid verticalAlign='middle' columns={6} divided centered>
             <GridColumn>
             <Card className='zoomfourth'>
@@ -75,7 +77,80 @@ export default class Page3 extends Component {
         </Grid>
 
     </div>
+    
    )
   }
 }
+*/
+import  React  from  'react';
+import  Carousel  from  'semantic-ui-carousel-react';
+import { Image, Button,Card,Icon } from  'semantic-ui-react'
+const  Page3  = () => {
+	let  elements  = [
+		{
+			render:()=>{
+				return(
+                <Card fluid color='black'>
+                <Image src='' wrapped ui={false} />
+                <Card.Content>
+                <Card.Header>Matthew</Card.Header>
+                <Card.Meta>
+                    <span className='date'>Joined in 2015</span>
+                </Card.Meta>
+                <Card.Description>
+                    Matthew is a musician living in Nashville.
+                </Card.Description>
+                </Card.Content>
+                <Card.Content extra>
+                <a>
+                    <Icon name='user' />
+                    22 Friends
+                </a>
+                </Card.Content>
+                </Card>
+                )
+			}
+		},
+		{
+			render:()=>{
+				return(
+                <Card fluid color='black'>
+                <Image src='' wrapped ui={false} />
+                <Card.Content>
+                <Card.Header>Bill</Card.Header>
+                <Card.Meta>
+                    <span className='date'>Joined in 2015</span>
+                </Card.Meta>
+                <Card.Description>
+                    Matthew is a musician living in Nashville.
+                </Card.Description>
+                </Card.Content>
+                <Card.Content extra>
+                <a>
+                    <Icon name='user' />
+                    22 Friends
+                </a>
+                </Card.Content>
+                </Card>
+                )
+			}
+	    },
+	]
+	return (
+		<div className='fourthPage'>
+			<Carousel
+                className='carousel'
+				elements  =  {  elements  }
+				duration  ={10000}
+				animation  ='fade left'
+				showNextPrev  =  {false}
+				showIndicators  ={true}
+			/>
+		</div>
+	)
+
+}
+export  default  Page3;
+
+
 
