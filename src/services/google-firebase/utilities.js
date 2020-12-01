61f0d9f1f83fd1a660c7bdc934d8a7fd0c3ecdac
Rahul Bera <bera2591@gmail.com>
2020-12-01 22:48:51 +0530

    utilities.js: Import firebase & create addNewUser function

---
+++ src/services/google-firebase/utilities.js
@@ -0,0 +1,49 @@
+import firebase from "firebase/app";
+
+// Add the Firebase products that you want to use
+import "firebase/auth";
+import "firebase/firestore";
+// Import helper functions
+import { generatePassword, convertDec2CustomBase26 } from './helper'
+
+// Firebase Setup
+const firebaseConfig = require('./firebase_config.json');
+firebase.initializeApp(firebaseConfig);
+
+// Firebase product objects
+var auth = firebase.auth();
+
+/**
+ * Create a new member account.
+ * @param {string} first_name The first name of the user.
+ * @param {string} last_name The last name or surname of the user.
+ * @param {number} year_of_joining The year in which the user joined the organization.
+ */
+export async function addNewUser(first_name, last_name, year_of_joining){
+    
+    // Generate Email
+    let emailDomain = '@tcr.in';
+    let username = first_name + '.' + last_name + year_of_joining.toString();
+    let email = username + emailDomain;
+    let replicas = 0; // Counter for number of people with same username
+    while ((await auth.fetchSignInMethodsForEmail(email)).length !== 0) {
+        replicas++;
+        var temp = convertDec2CustomBase26(replicas-1);
+        email = username + temp + emailDomain;
+    }
+    // Generate Password
+    let password = await generatePassword();
+    // Create User
+    auth.createUserWithEmailAndPassword(email, password)
+    .then((currentUser) => {
+        console.log(currentUser.user.uid);
+        auth.signOut();
+    })
+    .catch((error) => {
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        console.error(errorCode);
+        console.error(errorMessage);
+    });
+
+};
