a8105fe33012df74ef6a6644fccb5a4b65796ad1
Karan Yuvraj Singh <karan124d@gmail.com>
2020-12-01 17:29:47 +0530

    customblogauthor card

--- src/components/blog/MiscCard.jsx
+++ src/components/blog/MiscCard.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { Grid, Segment, Header,Image, Icon } from 'semantic-ui-react';
+import { Grid, Segment, Header,Image, Icon, Card, Button } from 'semantic-ui-react';
+import './misccard.css';
 
 export default class MiscCard extends Component {
   render() {
@@ -7,7 +8,36 @@ export default class MiscCard extends Component {
         <div className="postlayer1">
             <div className="postlayer2">
                 <div className="postlayer3">
-                    {this.props.mainContent}
+                    <div className="mainLayerFront">
+                        {this.props.mainContent}
+                        This is the front.
+                    </div>
+                    <div className="mainLayerBack" >
+                        <Card className='customBlogCard'>
+                        <Card.Content>
+                            <Image
+                            floated='right'
+                            size='mini'
+                            src='https://react.semantic-ui.com/images/avatar/large/steve.jpg'
+                            />
+                            <Card.Header>Steve Sanders</Card.Header>
+                            <Card.Meta>Friends of Elliot</Card.Meta>
+                            <Card.Description>
+                            Steve wants to add you to the group <strong>best friends</strong>
+                            </Card.Description>
+                        </Card.Content>
+                        <Card.Content extra>
+                            <div className='ui two buttons'>
+                            <Button basic color='green'>
+                                Approve
+                            </Button>
+                            <Button basic color='red'>
+                                Decline
+                            </Button>
+                            </div>
+                        </Card.Content>
+                        </Card>
+                    </div>
                 </div>
             </div>
         </div>
