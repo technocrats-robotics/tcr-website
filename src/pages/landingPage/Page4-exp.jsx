abd7a750533606821a89ff638a4f2b20005537e7
Ishika <ishika.u.naik@gmail.com>
2020-12-02 17:12:38 +0530

    carousel responsive

--- src/pages/landingPage/Page4-exp.jsx
+++ src/pages/landingPage/Page4-exp.jsx
@@ -1,14 +1,10 @@
 /*
-import { render } from '@testing-library/react'
-import React, { Component } from 'react';
-import {Grid, GridColumn, GridRow,Card,Icon, Image,Header} from 'semantic-ui-react';
+import React,{Component} from 'react';
+import {Carousel} from 'react-responsive-carousel';
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Card,Image,Grid, GridColumn, CardContent,Segment, GridRow,Header } from "semantic-ui-react";
 
-/**
-* @author
-* @function Page3
-**/
-/**** 
-export default class Page3 extends Component {
+export default class Page4 extends Component {
     render(){
   return(
     <div className="fourthPage">
@@ -82,6 +78,7 @@ export default class Page3 extends Component {
   }
 }
 */
+
 /*
 import  React  from  'react';
 import  Carousel  from  'semantic-ui-carousel-react';
@@ -161,27 +158,62 @@ import {Grid, GridColumn, GridRow,Card,Icon, Image,Header} from 'semantic-ui-rea
 
 /**
 * @author
-* @function Page4
+* @function Page3
 **/
 
-export default class Page4 extends Component {
+export default class Page3 extends Component {
     render(){
   return(
     <div className="fourthPage">
         <Grid>
-            <GridRow>
-                <GridColumn>
-                <Header as='h1' icon textAlign='center' inverted color='grey'>
-                    <Icon inverted name='users' circular />
-                    <Header.Content >Testimonials</Header.Content>
-                </Header>
-                <Carousel />
-                </GridColumn>
-            </GridRow>
+            <Grid.Row>
+                <Grid.Column>
+                    <Header as='h1' icon textAlign='center' inverted color='grey'>
+                        <Icon inverted name='users' circular />
+                        <Header.Content >Testimonials</Header.Content>
+                    </Header>
+                    <Carousel />
+                </Grid.Column>
+            </Grid.Row>
         </Grid>
+                <Carousel>
+                <div>
+                <Segment fluid inverted color='black'>
+                    <Grid stackable>
+                    <GridRow>
+                    <GridColumn mobile={16} tablet={8} computer={4} color='red'>
+                        <Image size='big' />
+                        <Header style={{color: 'white'}}></Header>
+                    </GridColumn>
+                    <GridColumn mobile={16} tablet={8} computer={4} color='pink'>
+                    </GridColumn>
+                    </GridRow>
+                    </Grid>
+                </Segment>
+                </div>
+                <div>
+                    <img src="assets/2.jpeg" />
+                    <p className="legend">Legend 2</p>
+                </div>
+                <div>
+                    <img src="assets/3.jpeg" />
+                    <p className="legend">Legend 3</p>
+                </div>
+                <div>
+                    <img src="assets/4.jpeg" />
+                    <p className="legend">Legend 4</p>
+                </div>
+                <div>
+                    <img src="assets/5.jpeg" />
+                    <p className="legend">Legend 5</p>
+                </div>
+                <div>
+                    <img src="assets/6.jpeg" />
+                    <p className="legend">Legend 6</p>
+                </div>
+            </Carousel>
     </div>
     
    )
   }
 }
-
--- src/pages/landingPage/Page4.jsx
+++ src/pages/landingPage/Page4.jsx
@@ -1,13 +1,17 @@
-import React,{Component} from 'react';
-import {Carousel} from 'react-responsive-carousel';
-import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
-import { Card,Image,Grid, GridColumn, CardContent,Segment, GridRow,Header } from "semantic-ui-react";
+/*
+import { render } from '@testing-library/react'
+import React, { Component } from 'react';
+import {Grid, GridColumn, GridRow,Card,Icon, Image,Header} from 'semantic-ui-react';
 
-export default class Page4 extends Component {
+/**
+* @author
+* @function Page3
+**/
+/**** 
+export default class Page3 extends Component {
     render(){
   return(
     <div className="fourthPage">
-<<<<<<< Updated upstream
         <Header textAlign='center' color='teal'>Testimonials</Header>
         <Grid verticalAlign='middle' columns={6} divided centered>
             <GridColumn>
@@ -152,69 +156,32 @@ export  default  Page3;
 
 import { render } from '@testing-library/react'
 import React, { Component } from 'react';
-import Carousel from './../../components/Carousel';
+import Carousel from '../../components/Carousel';
 import {Grid, GridColumn, GridRow,Card,Icon, Image,Header} from 'semantic-ui-react';
 
 /**
 * @author
-* @function Page3
+* @function Page4
 **/
 
-export default class Page3 extends Component {
+export default class Page4 extends Component {
     render(){
   return(
     <div className="fourthPage">
         <Grid>
-            <Grid.Row>
-                <Grid.Column>
-                    <Header as='h1' icon textAlign='center' inverted color='grey'>
-                        <Icon inverted name='users' circular />
-                        <Header.Content >Testimonials</Header.Content>
-                    </Header>
-                    <Carousel />
-                </Grid.Column>
-            </Grid.Row>
+            <GridRow>
+                <GridColumn>
+                <Header as='h1' icon textAlign='center' inverted color='grey'>
+                    <Icon inverted name='users' circular />
+                    <Header.Content >Testimonials</Header.Content>
+                </Header>
+                <Carousel />
+                </GridColumn>
+            </GridRow>
         </Grid>
-=======
-                <Carousel>
-                <div>
-                <Segment fluid inverted color='black'>
-                    <Grid stackable>
-                    <GridRow>
-                    <GridColumn mobile={16} tablet={8} computer={4} color='red'>
-                        <Image size='big' />
-                        <Header style={{color: 'white'}}></Header>
-                    </GridColumn>
-                    <GridColumn mobile={16} tablet={8} computer={4} color='pink'>
-                    </GridColumn>
-                    </GridRow>
-                    </Grid>
-                </Segment>
-                </div>
-                <div>
-                    <img src="assets/2.jpeg" />
-                    <p className="legend">Legend 2</p>
-                </div>
-                <div>
-                    <img src="assets/3.jpeg" />
-                    <p className="legend">Legend 3</p>
-                </div>
-                <div>
-                    <img src="assets/4.jpeg" />
-                    <p className="legend">Legend 4</p>
-                </div>
-                <div>
-                    <img src="assets/5.jpeg" />
-                    <p className="legend">Legend 5</p>
-                </div>
-                <div>
-                    <img src="assets/6.jpeg" />
-                    <p className="legend">Legend 6</p>
-                </div>
-            </Carousel>
->>>>>>> Stashed changes
     </div>
     
    )
   }
 }
+
