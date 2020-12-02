19f1a338edfd23cfc300e88dc676f4f5567071bc
Rahul Bera <bera2591@gmail.com>
2020-12-03 01:17:32 +0530

    utilities.js - Rename 'emailDomain' to 'email_domain' - Add parameters for 'addNewUser' function - Remove spaces from the names before using them in username - Import the 'Member' class - Use the 'createMemberDocument' to add members - Use 'titleCase' function from helper.js to convert resp strings

--- src/services/google-firebase/utilities.js
+++ src/services/google-firebase/utilities.js
@@ -3,39 +3,65 @@
 // Import firebase product objects
 import { auth } from './setup';
 // Import helper functions
-import { generatePassword, convertDec2CustomBase26 } from './helper';
+import { generatePassword, convertDec2CustomBase26, titleCase } from './helper';
+// Import Schema
+import Member from './models/members/member';
+
+
 
 /**
  * Create a new member account.
  * @param {string} first_name The first name of the user.
  * @param {string} last_name The last name or surname of the user.
+ * @param {string} registered_email A valid email that the user would like be contacted through.
+ * @param {string} branch Branch/Department the user belongs to.
  * @param {number} year_of_joining The year in which the user joined the organization.
  */
-export async function addNewUser(first_name, last_name, year_of_joining){
+export async function addNewUser(first_name, last_name, registered_email, branch, year_of_joining){
     
     // Generate Email
-    let emailDomain = '@tcr.in';
+    let email_domain = '@tcr.in';
+    let full_name = first_name + " " + last_name;
+    first_name = first_name.replace(/ /g, "");  // Remove spaces
+    last_name = last_name.replace(/ /g, "");    // Remove spaces
     let username = first_name + '.' + last_name + year_of_joining.toString();
-    let email = username + emailDomain;
+    let email = username + email_domain;
     let replicas = 0; // Counter for number of people with same username
     while ((await auth.fetchSignInMethodsForEmail(email)).length !== 0) {
         replicas++;
         var temp = convertDec2CustomBase26(replicas-1);
-        email = username + temp + emailDomain;
+        email = username + temp + email_domain;
     }
     // Generate Password
     let password = await generatePassword();
     // Create User
     auth.createUserWithEmailAndPassword(email, password)
-    .then((currentUser) => {
-        console.log(currentUser.user.uid);
+    .then(async (currentUser) => {
+        // console.log(currentUser.user.uid);   // Debugging
+        let newMember = new Member(currentUser.user.uid);
+        let isCreated = await newMember.createMemberDocument(
+            username.toLowerCase(),
+            titleCase(full_name),
+            registered_email.toLowerCase(),
+            titleCase(branch),
+            year_of_joining
+        );
+        if(!isCreated)
+            console.error('Something went wrong in addNewUser');
         auth.signOut();
+        console.log('Success'); // Debugging
     })
     .catch((error) => {
         var errorCode = error.code;
         var errorMessage = error.message;
+        console.error('Error in addNewUser');
         console.error(errorCode);
         console.error(errorMessage);
     });
 
 };
+
+/**
+ * References:
+ * https://www.geeksforgeeks.org/how-to-remove-spaces-from-a-string-using-javascript/
+ */
