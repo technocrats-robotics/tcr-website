d11be88a620796e9075cdc81c56741c3b6e40add
Shivansh <shivanshgoel21@gmail.com>
2020-12-04 11:31:10 +0530

    Menu options conditional rendering fix

--- src/userPanel/UserPanelNav.js
+++ src/userPanel/UserPanelNav.js
@@ -1,14 +1,30 @@
-import React from 'react'
+import React,{useEffect,useContext,useState} from 'react'
 import {NavLink} from "react-router-dom"
 
 
 //auth from services/google-firebase
 import {auth} from "../services/google-firebase/setup"
 
+// db from local firebase setup
+import {db} from "../services/google-firebase/setup"
+
+//GlobalUser from context
+import {GlobalUser} from "./UserPanel"
+
 //CSS
 import "./CSS/UserPanelNav.css"
 
 function UserPanelNav() {
+
+    const user=useContext(GlobalUser);
+    //User state variable for local use
+    const[User,setUser]=useState(user);
+
+    useEffect(() => {
+        db.collection('members').doc(user)
+        .onSnapshot(function(doc) {
+        setUser(doc.data());    
+    })})
 
     const handleSignOut=()=>{
         auth.signOut().then(function() {
@@ -19,21 +35,29 @@ function UserPanelNav() {
     }
 
     return (
-        <div class="ui massive menu">
-            <div class="ui inverted secondary pointing menu">
-            <img class="ui medium image" src="http://www.technocratsrobotics.in/images/technocrats.png" alt="logo"/>
+        <div className="ui massive menu">
+            <div className="ui inverted secondary pointing menu">
+            <img className="ui medium image" src="http://www.technocratsrobotics.in/images/technocrats.png" alt="logo"/>
                 <div className="item">
                     <NavLink activeClassName="active_link" exact to='/userPanel' >@ My Profile</NavLink>
                 </div>
-                <div className="item">
-                    <NavLink activeClassName="active_link" exact to='/userPanel/myPosts' > $ My Posts</NavLink>
-                </div>
-                <div className="item">
-                    <NavLink activeClassName="active_link" exact to='/userPanel/writePost' > ++ New Post</NavLink>
-                </div>
-                <div class="right item">
-                    <div class="item">
-                        <div class="ui primary button" onClick={handleSignOut}>Sign Out</div>
+                {
+                    User && User.blogAccess &&( 
+                            <div className="item">
+                                <NavLink activeClassName="active_link" exact to='/userPanel/myPosts' > $ My Posts</NavLink>
+                            </div>
+                    ) 
+                }
+                {
+                    User && User.blogAccess && (
+                            <div className="item">
+                                <NavLink activeClassName="active_link" exact to='/userPanel/writePost' > ++ New Post</NavLink>
+                            </div>
+                    )
+                }
+                <div className="right menu">
+                    <div className="item">
+                        <div className="ui primary button" onClick={handleSignOut}>Sign Out</div>
                     </div>
                 </div>
             </div>
