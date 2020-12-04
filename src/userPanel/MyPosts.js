f7c491d6a257f2210e5c7e493a704ded8842f532
Shivansh <shivanshgoel21@gmail.com>
2020-12-04 11:33:49 +0530

    MyPosts conditional render fix

--- src/userPanel/MyPosts.js
+++ src/userPanel/MyPosts.js
@@ -1,10 +1,31 @@
-import React from 'react'
+import React,{useContext,useState,useEffect} from 'react'
+
+import {GlobalUser} from "./UserPanel"
+
+//db from local firebase setup
+import {db} from "../services/google-firebase/setup"
 
 function MyPosts() {
+    //uid from context
+    const user=useContext(GlobalUser);
+    //after fetching user's information from firestore
+    const[User,setUser]=useState(false);
+
+    useEffect(() => {
+        db.collection('members').doc(user)
+        .onSnapshot(function(doc) {
+        setUser(doc.data());    
+    })})
+
     return (
         <div>
-            <h1>To display user posts</h1>
-            <p>Under Development!!</p>
+        {
+            (User && User.blogAccess)?(
+                <div>Success</div>
+            ):(
+                <h1>Access Denied!!</h1>
+            )
+        }
         </div>
     )
 }
