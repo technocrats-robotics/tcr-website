87acc4534ce46e610bf79a0d4a9f650907995ce6
Karan Yuvraj Singh <karan124d@gmail.com>
2020-12-04 19:50:44 +0530

    About Us Page
    * Created About Us containing 2 separate components for members and about team.
    * Created the cards to be displayed as team members

--- src/components/about_us/AboutUsCard.jsx
+++ src/components/about_us/AboutUsCard.jsx
@@ -1,27 +1,29 @@
 import React, { Component } from 'react';
 import { Grid, Segment, Header,Image, Icon, Card, Button } from 'semantic-ui-react';
+import './AboutUsCard.css';
 
 export default class AboutUsCard extends Component {
   render() {
     return(
         <div>
-            <Grid stackable relaxed centered columns={3} divided>
-              <Grid.Row>
-                  <MiscCard></MiscCard>
-                  <MiscCard></MiscCard>
-                  <MiscCard></MiscCard>
-              </Grid.Row>
-              <Grid.Row>
-                  <MiscCard></MiscCard>
-                  <MiscCard></MiscCard>
-                  <MiscCard></MiscCard>
-              </Grid.Row>
-              <Grid.Row>
-                  <MiscCard></MiscCard>
-                  <MiscCard></MiscCard>
-                  <MiscCard></MiscCard>
-              </Grid.Row>
-            </Grid>
+            <div class="aboutuscard">
+                    <div class="card__face card__face--front">             
+                        <Image className='faceimage' size='small' avatar src='https://react.semantic-ui.com/images/avatar/large/daniel.jpg' />
+                        <Header color='red' inverted>
+                            Daniel
+                            <Header.Subheader color='blue'>2017 CSE</Header.Subheader>
+                        </Header>
+                    </div>
+                    <div class="card__face card__face--back">
+                    <Header color='red' inverted>
+                            Daniel
+                            <Header.Subheader color='blue'>2017 CSE</Header.Subheader>
+                            <Header.Content as={'h6'} >
+                            By the same illusion which lis the horizon of the sea to the level of the spectator on a hil.lside, the sable.
+                            </Header.Content>
+                        </Header>
+                    </div>
+            </div>
         </div>
     );
     }
--- src/pages/about_us/Page1.jsx
+++ src/pages/about_us/Page1.jsx
@@ -1,11 +1,59 @@
 import React, { Component } from 'react';
 import { Grid, Segment, Header,Image } from 'semantic-ui-react';
+import AboutUsCard from '../../components/about_us/AboutUsCard';
 
 export default class Page1 extends Component {
+    memberDetails = [
+        {
+        username: 'daniel12',
+        name: 'Daniel',
+        registeredEmail: 'daniel12@gmail.com',
+        branch: 'CSE',
+        yearOfJoining: '2017',
+        isActive: 1,
+        blogAccess: 1,
+        about: {
+            experience: 'Has expertise in his area of interest.',
+        }
+    },
+        {
+        username: 'daniel12',
+        name: 'Daniel',
+        registeredEmail: 'daniel12@gmail.com',
+        branch: 'CSE',
+        yearOfJoining: '2017',
+        isActive: 1,
+        blogAccess: 1,
+        about: {
+            experience: 'Has expertise in his area of interest.',
+        }
+        },{
+            username: 'daniel12',
+            name: 'Daniel',
+            registeredEmail: 'daniel12@gmail.com',
+            branch: 'CSE',
+            yearOfJoining: '2017',
+            isActive: 1,
+            blogAccess: 1,
+            about: {
+                experience: 'Has expertise in his area of interest.',
+            }
+            }
+    ];
   render() {
     return(
         <div className='firstPage'>
-            Hello About Us
+            <Grid centered>
+                <Grid.Row columns='6'>
+                {
+                this.memberDetails.map((member)=>(
+                    <Grid.Column>
+                    <AboutUsCard></AboutUsCard>
+                    </Grid.Column>
+                ))
+                }
+                </Grid.Row>
+            </Grid>
         </div>
     );
     }
