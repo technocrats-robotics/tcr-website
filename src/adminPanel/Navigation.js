3cf10631c4f385d994d632fea810a52f2bc5ebc3
Shivansh <shivanshgoel21@gmail.com>
2020-12-04 16:30:05 +0530

    Navigation: console errors fix

--- src/adminPanel/Navigation.js
+++ src/adminPanel/Navigation.js
@@ -18,21 +18,21 @@ function Navigation() {
 
     return (
         <div className="navBar">
-            <div class="ui menu inverted">
-                <img class="ui tiny image admin__tcrLogo" src="http://www.technocratsrobotics.in/images/logo21.png"></img>
-                <div class="header item">
+            <div className="ui menu inverted">
+                <img className="ui tiny image admin__tcrLogo" src="http://www.technocratsrobotics.in/images/logo21.png"></img>
+                <div className="header item">
                    <h2>Technocrats Robotics</h2>
                 </div>
                 <NavLink exact className="nav item" to="/adminPanel/addMembers"> Add Members</NavLink>
                 <NavLink exact className="nav item" to="/adminPanel/manageMembers"> Manage Members </NavLink>
                 <NavLink exact className="nav item" to="/adminPanel/sendBulkMails">Send Emails </NavLink>
                 <NavLink exact className="nav item" to="/adminPanel/makeAnnouncement">Make Announcement </NavLink>    
-                <div class="right menu">
-                    <div class="item">
+                <div className="right menu">
+                    <div className="item">
                         <div> Welcome Admin !!</div>
                     </div>
-                    <div class="item">
-                        <div class="ui primary button" onClick={handleSignOut}>Sign Out</div>
+                    <div className="item">
+                        <div className="ui primary button" onClick={handleSignOut}>Sign Out</div>
                     </div>
                 </div>
             </div>
