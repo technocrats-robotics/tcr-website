f112a190007706496702b462ddcd5fd1e0b48850
Ishika <ishika.u.naik@gmail.com>
2020-12-05 18:11:12 +0530

    About us page - Team Details Cards
    Creation of team details cards and their layouts

--- src/pages/about_us/Page1.jsx
+++ src/pages/about_us/Page1.jsx
@@ -1,58 +1,23 @@
 import React, { Component } from 'react';
-import { Grid, Segment, Header,Image } from 'semantic-ui-react';
+import { Grid, Segment, Header,Image, GridRow, GridColumn } from 'semantic-ui-react';
 import AboutUsCard from '../../components/about_us/AboutUsCard';
+import InfoCard from '../../components/about_us/InfoCard';
+import InfoCard2 from '../../components/about_us/InfoCard2';
 
 export default class Page1 extends Component {
-    memberDetails = [
-        {
-        username: 'daniel12',
-        name: 'Daniel',
-        registeredEmail: 'daniel12@gmail.com',
-        branch: 'CSE',
-        yearOfJoining: '2017',
-        isActive: 1,
-        blogAccess: 1,
-        about: {
-            experience: 'Has expertise in his area of interest.',
-        }
-    },
-        {
-        username: 'daniel12',
-        name: 'Daniel',
-        registeredEmail: 'daniel12@gmail.com',
-        branch: 'CSE',
-        yearOfJoining: '2017',
-        isActive: 1,
-        blogAccess: 1,
-        about: {
-            experience: 'Has expertise in his area of interest.',
-        }
-        },{
-            username: 'daniel12',
-            name: 'Daniel',
-            registeredEmail: 'daniel12@gmail.com',
-            branch: 'CSE',
-            yearOfJoining: '2017',
-            isActive: 1,
-            blogAccess: 1,
-            about: {
-                experience: 'Has expertise in his area of interest.',
-            }
-            }
-    ];
   render() {
     return(
         <div className='firstPage'>
             <Grid centered>
-                <Grid.Row columns='6'>
-                {
-                this.memberDetails.map((member)=>(
-                    <Grid.Column>
-                    <AboutUsCard></AboutUsCard>
-                    </Grid.Column>
-                ))
-                }
-                </Grid.Row>
+                <GridRow>
+                    <InfoCard></InfoCard>
+                </GridRow>
+                <GridRow>
+                    <InfoCard2></InfoCard2>
+                </GridRow>
+                <GridRow>
+                    <InfoCard></InfoCard>
+                </GridRow>
             </Grid>
         </div>
     );
---
+++ src/pages/about_us/Page2.jsx
@@ -0,0 +1,61 @@
+import React, { Component } from 'react';
+import { Grid, Segment, Header,Image } from 'semantic-ui-react';
+import AboutUsCard from '../../components/about_us/AboutUsCard';
+
+export default class Page2 extends Component {
+    memberDetails = [
+        {
+        username: 'daniel12',
+        name: 'Daniel',
+        registeredEmail: 'daniel12@gmail.com',
+        branch: 'CSE',
+        yearOfJoining: '2017',
+        isActive: 1,
+        blogAccess: 1,
+        about: {
+            experience: 'Has expertise in his area of interest.',
+        }
+    },
+        {
+        username: 'daniel12',
+        name: 'Daniel',
+        registeredEmail: 'daniel12@gmail.com',
+        branch: 'CSE',
+        yearOfJoining: '2017',
+        isActive: 1,
+        blogAccess: 1,
+        about: {
+            experience: 'Has expertise in his area of interest.',
+        }
+        },{
+            username: 'daniel12',
+            name: 'Daniel',
+            registeredEmail: 'daniel12@gmail.com',
+            branch: 'CSE',
+            yearOfJoining: '2017',
+            isActive: 1,
+            blogAccess: 1,
+            about: {
+                experience: 'Has expertise in his area of interest.',
+            }
+            }
+    ];
+  render() {
+    return(
+        <div className='secondPage'>
+            <Header textAlign='center' inverted size='huge'>The Team</Header>
+            <Grid centered>
+                <Grid.Row columns='6'>
+                {
+                this.memberDetails.map((member)=>(
+                    <Grid.Column>
+                    <AboutUsCard></AboutUsCard>
+                    </Grid.Column>
+                ))
+                }
+                </Grid.Row>
+            </Grid>
+        </div>
+    );
+    }
+}
