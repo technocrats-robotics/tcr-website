2d047c67a8fa2d35b2580ce8b0e3c9d941bc0542
Karan Yuvraj Singh <karan124d@gmail.com>
2020-12-08 16:46:13 +0530

    Fixed CSS
    * Fixed extra margin and paddings
    * Fixed overrding classes

--- src/components/about_us/InfoCard.jsx
+++ src/components/about_us/InfoCard.jsx
@@ -7,7 +7,7 @@ export default class InfoCard extends Component {
     return(
         <div>
             <div class="card1">
-            <div class="thumbnail1"><img class="left1" src="https://cdn2.hubspot.net/hubfs/322787/Mychefcom/images/BLOG/Header-Blog/photo-culinaire-pexels.jpg"/></div>
+            <div class="thumbnail1"><Image class="left1" src="https://cdn2.hubspot.net/hubfs/322787/Mychefcom/images/BLOG/Header-Blog/photo-culinaire-pexels.jpg"/></div>
             <div class="right1">
                 <h1 className='title1'>What are we striving towards?</h1>
                 <p>Magnesium is one of the six essential macro-minerals that is required by the body for energy production and synthesis of protein and enzymes. It contributes to the development of bones and most importantly it is responsible for synthesis of your DNA and RNA. A new report that has appeared in theBritish Journal of Cancer, gives you another reason to add more magnesium to your diet...</p>
