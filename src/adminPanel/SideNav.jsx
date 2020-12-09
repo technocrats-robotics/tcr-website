f9bbe2fbad8b0dc6a224c25d69bb2600f4b7e779
Karan Yuvraj Singh <karan124d@gmail.com>
2020-12-09 19:15:41 +0530

    Hamburger Menu in admin panel

--- src/adminPanel/SideNav.jsx
+++ src/adminPanel/SideNav.jsx
@@ -2,29 +2,24 @@ import React from 'react'
 import { Header, Icon, Image, Menu, Segment, Sidebar } from 'semantic-ui-react'
 import {NavLink} from 'react-router-dom'
 
+
 const SideNav = (props) => (
     <Sidebar
       as={Menu}
       animation='overlay'
       icon='labeled'
       inverted
+      className='sidebarAdminPanel'
+      direction='right'
       vertical
       visible={props.visibility}
       width='thin'
     >
-      <Menu.Item as='a'>
       <NavLink exact className="nav item" to="/adminPanel/addMembers"> Add Members</NavLink>
-        <Icon name='home' />
-        Home
-      </Menu.Item>
-      <Menu.Item as='a'>
-        <Icon name='gamepad' />
-        Games
-      </Menu.Item>
-      <Menu.Item as='a'>
-        <Icon name='camera' />
-        Channels
-      </Menu.Item>
+      <NavLink exact className="nav item" to="/adminPanel/manageMembers"> Manage Members </NavLink>
+      <NavLink exact className="nav item" to="/adminPanel/sendBulkMails">Send Emails </NavLink>
+      <NavLink exact className="nav item" to="/adminPanel/makeAnnouncement">Make Announcement </NavLink>    
+                
     </Sidebar>
 )
 
