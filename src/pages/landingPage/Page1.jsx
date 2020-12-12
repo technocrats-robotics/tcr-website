ed4728db466b55db607b85ccb6b6ceb8d99bcf16
Rahul Bera <bera2591@gmail.com>
2020-12-12 19:01:00 +0530

    Page1.jsx: Use LogoController to animate the Logo

--- src/pages/landingPage/Page1.jsx
+++ src/pages/landingPage/Page1.jsx
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 
 import { Image, Grid, Header, Icon  } from "semantic-ui-react";
 import Dial from "../../components/Dial.jsx";
+import FlareComponent from 'flare-react';
+import LogoController from '../../components/landingPage/LogoController'
+
 
 export default class Page1 extends Component {
 
@@ -13,10 +16,11 @@ export default class Page1 extends Component {
                     {/* <div className='firstpagebackdiv'></div> */}
                     <Grid.Column mobile='12' className='leftMain' width='6' >
                         <div style={{textAlign:'center'}}>
-                    <Image className='TechLogoMainPage' size='large' src={'/TcrLogoClean.png'}></Image>
-                    <Header className='Title' size='medium'>Technocrats Robotics</Header>
-                    <div className='arrowDown'>
-                    <Icon className='arrow' color='yellow' size='huge' name='chevron down'></Icon> </div>                  
+                            <FlareComponent className="TechLogoMainPage" width={360} height={360} controller={new LogoController()} file="tcr_logo.flr"/>
+                            <Header className='Title' size='medium'>Technocrats Robotics</Header>
+                            <div className='arrowDown'>
+                                <Icon className='arrow' color='yellow' size='huge' name='chevron down'></Icon>
+                            </div>                  
                         </div>
                         </Grid.Column>
                     <Grid.Column only='computer'  width='4'>
