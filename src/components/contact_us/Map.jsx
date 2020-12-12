6a263d6483cebc48ac7e0d8a28d4225476b1a35b
Rahul Bera <bera2591@gmail.com>
2020-12-12 16:31:00 +0530

    Map.jsx: Use Google Maps embed for Location

--- src/components/contact_us/Map.jsx
+++ src/components/contact_us/Map.jsx
@@ -1,34 +1,18 @@
 import React, { Component } from 'react';
-import GoogleMapReact from 'google-map-react';
-import { Header, Icon } from 'semantic-ui-react';
-
-const AnyReactComponent = ({ text }) => <div>{text}</div>;
 
 export default class Map extends Component {
-    static defaultProps = {
-        center: {
-          lat: 59.95,
-          lng: 30.33
-        },
-        zoom: 11
-      };
     render() {
         return (
-            <div>
-            <div style={{ height: '60vh', width: '95%' }} className='map'>
-            <GoogleMapReact
-              bootstrapURLKeys= 'AIzaSyBdhEOY7h6IdQspIT8CNN004zR3zP4DD7M'
-              defaultCenter={this.props.center}
-              defaultZoom={this.props.zoom}
-            >
-              <AnyReactComponent
-                lat={59.955413}
-                lng={30.337844}
-                text="My Marker"
-              />
-            </GoogleMapReact>
-          </div>
-          </div>
+            <iframe 
+              title="Technocrats Robotics"
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3890.003857257919!2d80.15438921482068!3d12.843026990940345!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a525927afa6b01f%3A0x3c20a15cc9f64aad!2sTechnocrats%20Robotics!5e0!3m2!1sen!2sin!4v1607768880310!5m2!1sen!2sin" 
+              frameborder="0" 
+              className='map'
+              style={{ height: '60vh', width: '95%' }}
+              allowfullscreen="" 
+              aria-hidden="false" 
+              tabindex="0"
+            ></iframe>
         )
       }
 }
