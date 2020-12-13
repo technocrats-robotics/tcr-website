1996a32ffcfde134647451f112f4dad69e5f0ce3
Rahul Bera <bera2591@gmail.com>
2020-12-14 00:50:56 +0530

    AboutUsCard.jsx: Modify placeholder content - Replace data.branch with data.currentRole - Add space between the hyphen - Fix 'class should be className' warnings - Disable/Comment console logs

--- src/components/about_us/AboutUsCard.jsx
+++ src/components/about_us/AboutUsCard.jsx
@@ -5,24 +5,24 @@ import './AboutUsCard.css';
 export default class AboutUsCard extends Component {
     constructor(props){
         super(props);
-        console.log(props.data);
-        console.log("About Us!!")
+        // console.log(props.data);
+        // console.log("About Us!!")
     }
   render() {
     return(
-            <div class="aboutuscard">
-                    <div class="card__face card__face--front">             
+            <div className="aboutuscard">
+                    <div className="card__face card__face--front">             
                         <Image className='faceimage' size='small' avatar src={this.props.data.dpLink!=null?this.props.data.dpLink:'https://react.semantic-ui.com/images/avatar/large/daniel.jpg'} />
                         <Header inverted>
                             {this.props.data.name}
                             <Header.Subheader>
-                            {this.props.data.branch}-{this.props.data.yearOfJoining}</Header.Subheader>
+                            {this.props.data.currentRole} - {this.props.data.yearOfJoining}</Header.Subheader>
                         </Header>
                     </div>
-                    <div class="card__face card__face--back">
+                    <div className="card__face card__face--back">
                     <Header inverted>
                             {this.props.data.name}
-                            <Header.Subheader className='blue'>{this.props.data.branch}-{this.props.data.yearOfJoining}</Header.Subheader>
+                            <Header.Subheader className='blue'>{this.props.data.branch} - {this.props.data.yearOfJoining}</Header.Subheader>
                             <Header.Content as={'h6'} >
                             {this.props.data.about.experience}
                             </Header.Content>
