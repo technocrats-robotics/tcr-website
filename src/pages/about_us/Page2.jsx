a17a588de0df6b3d976125a9864fb845bba1eca8
Rahul Bera <bera2591@gmail.com>
2020-12-14 01:01:57 +0530

    Modify Team Display procedure - Remove dummy memberDetails - Set state.data=[] - Make sure 'getMemberData' is only called once - Seperate the UI part from 'getMemberData' to 'getSelectedTeam' - Use 'getSelectedTeam' as callback in setState for activeItem -  Call 'getSelectedTeam' once documents are retreived - Add key for similar children in nodes - Use same logic & structure for Page2.jsx & TeamMembers.jsx

--- src/pages/about_us/Page2.jsx
+++ src/pages/about_us/Page2.jsx
@@ -9,80 +9,80 @@ import Role from '../../services/google-firebase/models/members/role'
 
 var time = new Date();
 export default class Page2 extends Component {
-    memberDetails = [
-        {
-        username: 'daniel12',
-        name: 'Daniel',
-        registeredEmail: 'daniel12@gmail.com',
-        branch: 'CSE',
-        yearOfJoining: '2017',
-        isActive: 1,
-        blogAccess: 1,
-        about: {
-            experience: 'Has expertise in his area of interest.',
-        }
-    },
-    ];
-    state = { activeItem: '2020', data:this.memberDetails }
+    state = { 
+        activeItem: 
+            (time.getMonth() > 2)? 
+            (time.getFullYear()+1).toString(): 
+            time.getFullYear().toString(), 
+        data:[], yearHeaders: [] 
+    };
     
     handleItemClick = (e, { name }) => {
-        this.setState({ activeItem: name })
-        this.getMemberData()
+        this.setState({ activeItem: name }, () => this.getSelectedTeam());
     }
+
     async getMemberData(){
-        var database = await db.collection(Member.collectionName).get()
-        let tempArray = []
-        database.forEach((ele)=>{
-            let temp = ele.data()
-            console.log(ele.data().roles);
-            console.log(parseInt(this.state.activeItem))
-            if(ele.data().roles[parseInt(this.state.activeItem)+1] == Role.LEAD){
-            tempArray.push(ele.data())
-            console.log('pushed')
-        }
-        })
+        db.collection(Member.collectionName).get()
+        .then((documents) => {
+            this.documents = documents;
+            this.getSelectedTeam();
+        });
+        // console.log('getMemberData');
+    }
+
+    getSelectedTeam() {
+        let tempArray = [];
+        // console.log('getSelectedTeam '+this.state.activeItem);
+        this.documents.forEach((doc)=>{
+            let details = doc.data();
+            let selectedTeam = parseInt(this.state.activeItem);
+            let roleForSelectedTeamYear = doc.data().roles[selectedTeam] ?? Role.ALUMNI;
+            details['currentRole'] = roleForSelectedTeamYear;
+            if(
+                roleForSelectedTeamYear !== Role.ALUMNI && 
+                (
+                    roleForSelectedTeamYear === Role.CAPTAIN || 
+                    roleForSelectedTeamYear === Role.LEAD
+                )
+            ){
+                tempArray.push(details);
+            }
+        });
         this.setState({data: tempArray});
     }
     componentDidMount(){
-        this.getMemberData()
+        this.getMemberData();
+        let currentYear = (time.getMonth() > 2)? time.getFullYear()+1: time.getFullYear();
+        let tempyearHeaders = [currentYear];
+        // console.log(currentYear)
+        for(let i = currentYear ;i>2013;i--){
+            tempyearHeaders.push(i);
+        }
+        tempyearHeaders = [...new Set(tempyearHeaders)];
+        this.setState({yearHeaders:tempyearHeaders});
     }
   render() {
-    const { activeItem } = this.state
+    const { activeItem } = this.state;
     return(
         <div className='secondAboutPage'>
             <Header textAlign='center' inverted size='huge'>The Team Leads</Header>
             <Menu attached='top' tabular inverted pointing secondary className='blogMenuTop' size='huge' fluid>
-                        <Menu.Item inverted
-                            name='2020'
-                            active={activeItem === '2020'}
-                            onClick={this.handleItemClick}
-                        />
-                        <Menu.Item inverted
-                            name='2019'
-                            active={activeItem === '2019'}
-                            onClick={this.handleItemClick}
-                        />
-                        <Menu.Item inverted
-                            name='2018'
-                            active={activeItem === '2018'}
-                            onClick={this.handleItemClick}
-                        />
-                        <Menu.Item inverted
-                            name='2017'
-                            active={activeItem === '2017'}
-                            onClick={this.handleItemClick}
-                        />
-                        {/* {
-                            for(let p = time.getFullYear(); p > time.getFullYear()-4;p--){
-                                console.log('blah')
-                            }
-                        } */}
+            {
+                this.state.yearHeaders.map((year)=>{  
+                        return(<Menu.Item inverted="true"
+                        name={year.toString()}
+                        key={year.toString()}
+                        active={activeItem.toString() === year.toString()}
+                        onClick={this.handleItemClick}
+                    />)
+                })
+            }
             </Menu>
             <Grid centered doubling stackable>
                 <Grid.Row columns={6}>
                 {
                 this.state.data.map((member)=>(
-                    <Grid.Column computer= {4} className='justToAlignMemberCards'>
+                    <Grid.Column key={member.username} computer= {4} className='justToAlignMemberCards'>
                     <AboutUsCard data={member}></AboutUsCard>
                     </Grid.Column>
                 ))
--- src/pages/about_us/TeamMembers.jsx
+++ src/pages/about_us/TeamMembers.jsx
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
 import { Grid, Header,Menu, Label, Segment, Icon } from 'semantic-ui-react';
 import AboutUsCard from '../../components/about_us/AboutUsCard';
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Home from '../landingPage/LandingPage';
 import {db} from '../../services/google-firebase/setup';
 import Member from '../../services/google-firebase/models/members/member'
 import Role from '../../services/google-firebase/models/members/role'
@@ -10,68 +8,67 @@ import Role from '../../services/google-firebase/models/members/role'
 
 var time = new Date();
 export default class TeamMembers extends Component {
-    memberDetails = [
-        {
-        username: 'daniel12',
-        name: 'Daniel',
-        registeredEmail: 'daniel12@gmail.com',
-        branch: 'CSE',
-        yearOfJoining: '2017',
-        isActive: 1,
-        blogAccess: 1,
-        about: {
-            experience: 'Has expertise in his area of interest.',
-        }
-    },
-    ];
-    state = { activeItem: time.getFullYear(), data:this.memberDetails, yearHeaders:[] }
+    state = { 
+        activeItem: (time.getMonth() > 2)? time.getFullYear()+1: time.getFullYear(), 
+        data:[], yearHeaders:[] 
+    }
     
     handleItemClick = (e, { name }) => {
-        this.setState({ activeItem: name })
-        this.getMemberData()
+        this.setState({ activeItem: name }, () => this.getSelectedTeam());
     }
+
     async getMemberData(){
-        var database = await db.collection(Member.collectionName).get()
-        let tempArray = []
-        database.forEach((ele)=>{
-            let temp = ele.data()
-            console.log(ele.data().roles);
-            console.log(parseInt(this.state.activeItem))
-            if(ele.data().yearOfJoining === parseInt(this.state.activeItem)){
-                tempArray.push(ele.data())
+        db.collection(Member.collectionName).get()
+        .then((documents) => {
+            this.documents = documents;
+            this.getSelectedTeam();
+        });
+        // console.log('getMemberData');
+    }
+
+    getSelectedTeam(){
+        let tempArray = [];
+        this.documents.forEach((doc)=>{
+            let details = doc.data();
+            let selectedTeam = parseInt(this.state.activeItem);
+            let roleForSelectedTeamYear = doc.data().roles[selectedTeam] ?? Role.ALUMNI;
+            details['currentRole'] = roleForSelectedTeamYear;
+            if(roleForSelectedTeamYear !== Role.ALUMNI){
+                tempArray.push(details);
             }
         })
         this.setState({data: tempArray});
     }
     componentDidMount(){
-        this.getMemberData()
-        var currentYear = time.getFullYear()
-        var tempyearHeaders = [currentYear]
+        this.getMemberData();
+        let currentYear = (time.getMonth() > 2)? time.getFullYear()+1: time.getFullYear();
+        let tempyearHeaders = [currentYear];
         // console.log(currentYear)
-        for(let i = currentYear ;i>currentYear-4;i--){
+        for(let i = currentYear ;i>2013;i--){
             tempyearHeaders.push(i);
         }
         tempyearHeaders = [...new Set(tempyearHeaders)]
         this.setState({yearHeaders:tempyearHeaders});
     }
+
     handleBack = () =>{
-        this.props.history.push('/')
+        this.props.history.push('/');
     }
   render() {
-    const { activeItem } = this.state
+    const { activeItem } = this.state;
     return(
         <div className='secondAboutPage'><Segment inverted>
             <Header textAlign='center' inverted size='huge'>The Team</Header>
-                <Label onClick={this.handleBack} as='a' color='red' ribbon='left' attached='top left'>
+                <Label onClick={this.handleBack} as='a' color='red' attached='top left'>
                     <Icon name='left arrow' /> Back
                 </Label>        
             <Menu attached='top' tabular inverted pointing secondary className='blogMenuTop' size='huge' fluid>
                         {
-                        this.state.yearHeaders.map((year)=>{ 
-                            console.log(year)     
-                                return(<Menu.Item inverted
-                                name={year}
-                                active={activeItem === year}
+                        this.state.yearHeaders.map((year)=>{  
+                            return(<Menu.Item inverted="true"
+                                name={year.toString()}
+                                key={year.toString()}
+                                active={activeItem.toString() === year.toString()}
                                 onClick={this.handleItemClick}
                             />)
                         })
@@ -81,7 +78,7 @@ export default class TeamMembers extends Component {
                 <Grid.Row columns={6}>
                 {
                 this.state.data.map((member)=>(
-                    <Grid.Column computer= {4} className='justToAlignMemberCards'>
+                    <Grid.Column key={member.username} computer= {4} className='justToAlignMemberCards'>
                     <AboutUsCard data={member}></AboutUsCard>
                     </Grid.Column>
                 ))
