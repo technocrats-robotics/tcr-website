4dda3d0205121c57c0b8f8cc1f463daaaf39dc94
Shivansh <shivanshgoel21@gmail.com>
2020-12-15 02:14:15 +0530

    change Gallery from class component to functional component, applied red spinner loading screen.

---
+++ src/components/SpinnerLoadingScreen/Loader.js
@@ -0,0 +1,18 @@
+import React,{useState} from 'react'
+import {Icon} from 'semantic-ui-react'
+import "./Loader.css"
+
+function Loader() {
+    const[loading,setLoading]=useState();
+    return [
+            (loading) ?(
+                <div className='loadScreen'>
+                    <Icon size='massive' color='red' loading name='spinner' />
+                </div>
+            ):(null),
+            ()=>{setLoading(true)},
+            ()=>(setLoading(false)) 
+    ]
+}
+
+export default Loader
