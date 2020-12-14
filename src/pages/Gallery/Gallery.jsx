d17cc8398f8c44f50a4eebb95fcd277601a0eebf
Rahul Bera <bera2591@gmail.com>
2020-12-14 18:02:58 +0530

    Gallery.jsx: Fetch image data from firestore - Remove unused constructor - Remove redundant imports - Fix warnings - Retrieve image links from Firestore DB to display in carousel

--- src/pages/Gallery/Gallery.jsx
+++ src/pages/Gallery/Gallery.jsx
@@ -1,64 +1,45 @@
 import React, { Component } from 'react';
-import { Grid, Image, Dimmer, Loader, Segment, Label, Icon } from 'semantic-ui-react';
+import { Segment, Label } from 'semantic-ui-react';
 import './Gallery.css';
 import ImageGallery from 'react-image-gallery';
 import { Breadcrumb } from 'semantic-ui-react';
-
-const images = [
-  {
-    original: 'https://technocrats-robotics.github.io/website/images/dragking.jpeg',
-    thumbnail: 'https://picsum.photos/id/1018/250/150/',
-  },
-  {
-    original: 'https://technocrats-robotics.github.io/website/Gallery/images/thumbs/11.jpeg',
-    thumbnail: 'https://picsum.photos/id/1015/250/150/',
-  },
-  {
-    original: 'https://technocrats-robotics.github.io/website/Gallery/images/fulls/01.jpeg',
-    thumbnail: 'https://picsum.photos/id/1019/250/150/',
-  },
-  {
-    original: 'https://technocrats-robotics.github.io/website/Gallery/images/fulls/01.jpeg',
-    thumbnail: 'https://technocrats-robotics.github.io/website/Gallery/images/fulls/01.jpeg',
-  },
-  {
-    original: 'https://technocrats-robotics.github.io/website/Gallery/images/fulls/01.jpeg',
-    thumbnail: 'https://technocrats-robotics.github.io/website/Gallery/images/fulls/01.jpeg',
-  },
-  {
-    original: 'https://technocrats-robotics.github.io/website/Gallery/images/fulls/01.jpeg',
-    thumbnail: 'https://technocrats-robotics.github.io/website/Gallery/images/fulls/01.jpeg',
-  },
-  {
-    original: 'https://technocrats-robotics.github.io/website/Gallery/images/fulls/01.jpeg',
-    thumbnail: 'https://technocrats-robotics.github.io/website/Gallery/images/fulls/01.jpeg',
-  },
-];
+import { db } from '../../services/google-firebase/setup';
 
 export default class Gallery extends Component {
-    constructor(props){
-        super(props);
-    }
-    
-    handleBack = () =>{
-      this.props.history.push('/')
+  state = { images: [], }
+
+  componentDidMount() {
+    db.collection('content/gallery/images').get()
+    .then((images) => {
+      images.forEach((image) => {
+        let galleryImage = { 
+          original: image.data().link,
+          thumbnail: image.data().link,
+        };
+        this.state.images.push(galleryImage);
+        this.setState(this.state);
+      });      
+    });
+  }
+  
+  handleBack = () =>{
+    this.props.history.push('/')
   }
         
   render() {
 
-const sections = [
-  { key: 'Home', content: 'Home', link: true, onClick:this.handleBack },
-  { key: 'Gallery', content: 'Gallery', active: true },
-]
+    const sections = [
+      { key: 'Home', content: 'Home', link: true, onClick:this.handleBack },
+      { key: 'Gallery', content: 'Gallery', active: true },
+    ]
 
     return(
         <div className='galleryDiv'>
           <Segment inverted>
-          <Label as='a' color='red' ribbon='left' attached='top left'>
-
-  <Breadcrumb icon='right angle' sections={sections} />
-                </Label> 
-            <ImageGallery items={images} showFullscreenButton={false} showPlayButton={false} showNav={false} showBullets={true} />;
+            <Label color='red' attached='top left'>
+              <Breadcrumb icon='right angle' sections={sections} />
+            </Label> 
+            <ImageGallery items={this.state.images} showFullscreenButton={false} showPlayButton={false} showNav={false} showBullets={true} />
           </Segment>
         </div>
     );
