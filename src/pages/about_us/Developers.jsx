4cc2262038d2967a1fb8ad61051e99e47a8aa887
Karan Yuvraj Singh <karan124d@gmail.com>
2020-12-15 20:33:38 +0530

    developer space

--- src/pages/about_us/Developers.jsx
+++ src/pages/about_us/Developers.jsx
@@ -7,6 +7,46 @@ import DeveloperCard from '../../components/about_us/DeveloperCard';
 
 
 var time = new Date();
+function dragElement(elmnt) {
+  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+  if (document.getElementById(elmnt.id + "header")) {
+    // if present, the header is where you move the DIV from:
+    document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
+  } else {
+    // otherwise, move the DIV from anywhere inside the DIV:
+    elmnt.onmousedown = dragMouseDown;
+  }
+
+  function dragMouseDown(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // get the mouse cursor position at startup:
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    document.onmouseup = closeDragElement;
+    // call a function whenever the cursor moves:
+    document.onmousemove = elementDrag;
+  }
+
+  function elementDrag(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // calculate the new cursor position:
+    pos1 = pos3 - e.clientX;
+    pos2 = pos4 - e.clientY;
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    // set the element's new position:
+    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+  }
+
+  function closeDragElement() {
+    // stop moving when mouse button is released:
+    document.onmouseup = null;
+    document.onmousemove = null;
+  }
+}
 export default class TeamMembers extends Component {
     state = { 
         activeItem: (time.getMonth() > 2)? time.getFullYear()+1: time.getFullYear(), 
@@ -40,9 +80,17 @@ export default class TeamMembers extends Component {
             let techIcons = document.getElementsByClassName('techIcons')
             for(let i = 0; i< techIcons.length; i++){
             document.getElementsByClassName('techIcons')[i].style.opacity = '1.0';
+            setInterval(()=>{
+                document.getElementsByClassName('techStack')[0].style.transition = "0s";
+            },1000)
         }
         }, 1000);
+        
+dragElement(document.getElementsByClassName("techStack")[0]);
+
     }
+
+    
 
     handleBack = () =>{
         this.props.history.push('/');
@@ -69,7 +117,10 @@ export default class TeamMembers extends Component {
                 ))
             }
                 </Grid.Row>
-                <div className='techStack'>
+                <div className='techStack' stackable>
+                    <div className='disclaim'>
+                    Handle With Care[Draggable]
+                    </div>
                         <Icon color='yellow' className='techIcons' name='node' size='huge' />
                         <Icon color='yellow' className='techIcons' name='react' size='huge' />
                         <Icon color='yellow' className='techIcons' name='fire' size='huge' />
