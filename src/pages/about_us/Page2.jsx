7c8291af29c4f6611cbb570e99dc1b5b4cd0d0c7
Rahul Bera <bera2591@gmail.com>
2020-12-16 20:01:30 +0530

    Page2.jsx: Remove duplicate retrieval of data -  To avoid additional reads from firestore, remove the code from 'getMemberData' method that retrives the same data twice from Firestore

--- src/pages/about_us/Page2.jsx
+++ src/pages/about_us/Page2.jsx
@@ -22,18 +22,6 @@ export default class Page2 extends Component {
     }
 
     async getMemberData(){
-        var database = await db.collection(Member.collectionName).get()
-        let tempArray = []
-        database.forEach((ele)=>{
-            let temp = ele.data()
-            // console.log(ele.data().roles);
-            // console.log(parseInt(this.state.activeItem))
-            let person = temp.roles[parseInt(this.state.activeItem)+1];
-            if(person === Role.LEAD || person === Role.CAPTAIN){
-            tempArray.push(temp)
-            // console.log('pushed')
-        }
-        })
         db.collection(Member.collectionName).get()
         .then((documents) => {
             this.documents = documents;
@@ -68,6 +56,7 @@ export default class Page2 extends Component {
         let num = Math.floor(Math.random() * (4));
         return colors[num]
     }
+
     componentDidMount(){
         this.getMemberData();
         let currentYear = (time.getMonth() > 2)? time.getFullYear()+1: time.getFullYear();
@@ -84,47 +73,47 @@ export default class Page2 extends Component {
                 let color=this.randomColor()
                 if(imgBg[i].style.boxShadow === ""){
                     imgBg[i].style.boxShadow = "0px 5px 4px 0px "+ color;
-                            }
-                        }
-                },1000)
-    }
-  render() {
-    const { activeItem } = this.state;
-    return(
-        <div className='secondAboutPage'>
-            <Divider horizontal inverted>Team Leads</Divider>
-            <Menu attached='top' tabular inverted pointing secondary className='blogMenuTop' size='huge' fluid>
-            {
-                this.state.yearHeaders.map((year)=>{  
-                        return(<Menu.Item inverted="true"
-                        name={year.toString()}
-                        key={year.toString()}
-                        active={activeItem.toString() === year.toString()}
-                        onClick={this.handleItemClick}
-                    />)
-                })
-            }
-            </Menu>
-            <Grid centered doubling stackable>
-                <Grid.Row columns={6}>
-                {
-                this.state.data.map((member)=>(
-                    <Grid.Column key={member.username} computer= {4} className='justToAlignMemberCards'>
-                    <AboutUsCard data={member}></AboutUsCard>
-                    </Grid.Column>
-                ))
                 }
-                </Grid.Row>
-                                  
-                <Grid.Row>
-                <div>
-                    <Link to="/Team">
-                    <Button color='yellow' basic>View Complete Team</Button>
-                    </Link>
-                </div>
-                </Grid.Row>
-            </Grid>
-        </div>
-    );
+            }
+        },1000);
+    }
+    render() {
+        const { activeItem } = this.state;
+        return(
+            <div className='secondAboutPage'>
+                <Divider horizontal inverted>Team Leads</Divider>
+                <Menu attached='top' tabular inverted pointing secondary className='blogMenuTop' size='huge' fluid>
+                {
+                    this.state.yearHeaders.map((year)=>{  
+                            return(<Menu.Item inverted="true"
+                            name={year.toString()}
+                            key={year.toString()}
+                            active={activeItem.toString() === year.toString()}
+                            onClick={this.handleItemClick}
+                        />)
+                    })
+                }
+                </Menu>
+                <Grid centered doubling stackable>
+                    <Grid.Row columns={6}>
+                    {
+                    this.state.data.map((member)=>(
+                        <Grid.Column key={member.username} computer= {4} className='justToAlignMemberCards'>
+                        <AboutUsCard data={member}></AboutUsCard>
+                        </Grid.Column>
+                    ))
+                    }
+                    </Grid.Row>
+                                    
+                    <Grid.Row>
+                    <div>
+                        <Link to="/Team">
+                        <Button color='yellow' basic>View Complete Team</Button>
+                        </Link>
+                    </div>
+                    </Grid.Row>
+                </Grid>
+            </div>
+        );
     }
 }
