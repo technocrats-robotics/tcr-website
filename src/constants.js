0857204f549d9f1b5bc2c7598a34e4a5f2aa56fe
Rahul Bera <bera2591@gmail.com>
2020-12-16 20:36:57 +0530

    constants.js: Add 'getCurrentTeamYear' function

--- src/constants.js
+++ src/constants.js
@@ -1,1 +1,21 @@
 export var admin_email = 'the.mortal.god@tcr.in';
+
+/**
+ * Get the team year in string
+ * 
+ * For instance: If the year is 2020 & the month is later than February,
+ * that implies that the team now is preparing for an event in 2021. 
+ * So, the current team year should be 2021. 
+ * Similarly, if the month is now is January or February & the year is 2020, 
+ * that implies the team is about to participate for an event in 2020.
+ * So, the current team year should be 2020.
+ * @returns string
+ * @note The month taken here is 2, because power transfer generally happens
+ * in the month of February, after which the new recruits are let in.
+ */
+export var getCurrentTeamYear = () => {
+    let time = new Date();
+    return (time.getMonth() > 2) ? 
+        (time.getFullYear()+1).toString(): 
+        time.getFullYear().toString();
+}
