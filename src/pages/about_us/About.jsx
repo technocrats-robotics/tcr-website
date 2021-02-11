318bd0130ab237f3f18c9fdc85ace5bcb94247cd
Karan Yuvraj Singh <karan124d@gmail.com>
2021-02-12 01:35:53 +0530

    Added Collapse/Expand in Gallery

--- src/pages/about_us/About.jsx
+++ src/pages/about_us/About.jsx
@@ -4,20 +4,32 @@ import Page1 from './Page1';
 import Page2 from './Page2';
 import TeamMembers from './TeamMembers';
 import './About.css';
+var x = window.matchMedia("(max-width: 700px)")
+
 
 export default class About extends Component {
     state = { 
         activeItem: 'collapse', 
     };
+    
     handleCollapse = ()=> {
         document.getElementsByClassName('secondAboutPage')[0].scrollIntoView({behavior:'smooth'});
         this.setState({activeItem:'collapse'})
-        setTimeout(()=>{window.scrollBy({top:150,behavior:'smooth'})},1000)
+        // if(!x.matches){
+        // setTimeout(()=>{window.scrollBy({top:150,behavior:'smooth'})},1000)
+        // }
+    }
+    handleExpand = ()=> {
+        document.getElementsByClassName('secondAboutPage')[0].scrollIntoView({behavior:'smooth'});
+        this.setState({activeItem:'expand'})
+        // if(!x.matches){
+        //     setTimeout(()=>{window.scrollBy({top:150,behavior:'smooth'})},1000)
+        //     }
     }
   render() {
     // return(
             switch (this.state.activeItem) {
-                case "gallery":
+                case "expand":
                     return(
                     <div>
                         <Page1></Page1>
@@ -30,7 +42,7 @@ export default class About extends Component {
                     <div>
                         <Page1 />;
                         <Page2 />;
-                        <Button onClick={()=>{this.setState({activeItem:'gallery'})}} color='yellow' basic>Expand</Button>
+                        <Button className='ExpandAboutButton' onClick={this.handleExpand} color='yellow' basic>Expand</Button>
                     </div>
                     )
             }
