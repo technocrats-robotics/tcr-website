4c9c965c86520616ea9e978a9496d99bc9a1ecba
ishivanshgoel <shivanshgoel21@gmail.com>
2022-10-03 17:49:48 +0530

    fixes #128

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,11 +1,16 @@
 import React, { Component } from "react";
 import "./css/landing.css";
-import { AnimatedBannerPage, WhoAreWePage, GalleryPage, AboutUsAndAcheivements, ContactUs } from "./landingPage/";
+import {
+  AnimatedBannerPage,
+  WhoAreWePage,
+  GalleryPage,
+  AboutUsAndAcheivements,
+  ContactUs,
+} from "./landingPage/";
 import About from "./landingPage/About";
 import { SideNavMob } from "../components";
 import { Icon, Button, Divider } from "semantic-ui-react";
 import { db } from "../services/google-firebase/setup";
-
 
 document.addEventListener("DOMContentLoaded", () => {
   setInterval(() => {
@@ -46,22 +51,11 @@ export default class LandingPage extends Component {
   }
   handleScrollingDown = (e, { calculations }) => {
     console.log(calculations);
-    // let nothing = (pixs > 5 && pixs<700 && dir=='down') ? document.getElementsByClassName('secondPage')[0].scrollIntoView({behavior:'smooth'})  : console.log('none')
-    // nothing = (pixs > 1300 && pixs<1500 && dir=='down') ? window.scrollBy(0,300) : console.log('none')
-    // nothing = (pixs > 3000 && pixs<3500 && dir=='down') ? window.scrollBy(0,720) : console.log('none')
   };
   async componentDidMount() {
     document.getElementsByClassName("firstPageMain")[0].style.backgroundImage =
       "url('/landingPageBackground.png')";
     this.getPageData();
-    // window.onscroll = () => {
-    //     var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
-    //     var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-    //     var scrolled = (winScroll / height) * 100;
-    //     // document.getElementById("myBar").style.width = scrolled + "%";
-    //     this.setState({percent: scrolled});
-    //     console.log(scrolled);
-    //     } ;
   }
   render() {
     return (
